81d0c864d86e41a3ffc22752644b4a843ace12ec
Josue-Molina <josue.molina02@inacapmail.cl>
2025-11-26 07:18:39 -0300

    Copia de codigo

--- app/src/main/java/com/example/lecturadeenergia/MainActivity.kt
+++ app/src/main/java/com/example/lecturadeenergia/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.lecturadeenergia.NavegacionControladaDeLaApp.NavegacionSegunCargo
 
+//TEST
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
